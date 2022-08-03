4620ae4dc6073e44ebbd8462f098483b45c14c0d
inheritance10 <ali552001a@gmail.com>
2022-08-03 19:04:52 +0300

    Initial commit

---
+++ ecomm-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        if($user){
+            //echo $user;
+            return $user;
+        }
+
+
+    }
+}
